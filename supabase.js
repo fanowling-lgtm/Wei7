10c95b9bab7f0b398e24d6b185f9d353cd6c5f0c
fanowling-lgtm <fanowling@gmail.com>
2026-01-18 09:56:41 -0500

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+<script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
+<script>
+const supabase = supabase.createClient(
+  "https://lkeoattyhyhedxwrglqp.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxrZW9hdHR5aHloZWR4d3JnbHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3NDAzNTIsImV4cCI6MjA4NDMxNjM1Mn0.PrCLodlhHQZbzNl6zoQTjRy2QyLuuZ5VZgHBQRmQwUs"
+);
+</script>
